b4b6c8c7c9583e8128093fa7c05d45ad2db01df5
meghanag12 <112290030+meghanag12@users.noreply.github.com>
2024-10-01 13:32:47 -0400

    added the React component

--- wst/myapp/urls.py
+++ wst/myapp/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"), 
     path("", views.stats_page, name = "stats_page"),
-    path("<char:swimmer_id>/", views.swim_stats, name = "swim_stats"), 
+    path("<char:swimmer_id>/", views.swim_stats, name = "swim_stats"),
+    path("<char:swimmer_id>/raw_data", views.raw_data, name = "raw_data"), 
+    path("<char:swimmer_id>/graph_data", views.graph_data, name = "graph_data") 
 ]
 
 
--- wst/myapp/views.py
+++ wst/myapp/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from . models import Swimmer 
 from . models import RecordMagnitude
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world")
+    return HttpResponse("Magnitude Recorder")
 
 def stats_page(request):
     return HttpResponse("Search Swimmer Stats")
 
 def swim_stats(request, swimmer_id):
     mag_stats_for_swimmer = RecordMagnitude.objects.filter(retrieve_swimmer = swimmer_id).values_list('magnitude', flat = True)
-    return mag_stats_for_swimmer
+    template = loader.get_template("myapp/swim_stats")
+    context = {
+        "mag_stats_for_swimmer": mag_stats_for_swimmer, 
+    }
+    return HttpResponse(template.render(context,request))
+
+def raw_data(request, swimmer_id):
+    return HttpResponse("Hello World")
+def graph_data(request, swimmer_id):
+    return HttpResponse("Hello World")
 
     
