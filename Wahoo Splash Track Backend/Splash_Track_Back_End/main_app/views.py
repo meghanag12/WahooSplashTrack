14322665a7825387485383a2f39c34b864c64706
Preston <xew5be@virginia.edu>
2024-10-09 16:09:42 -0400

    added better error reception for the POST command for starts

--- Wahoo Splash Track Backend/Splash_Track_Back_End/main_app/views.py
+++ Wahoo Splash Track Backend/Splash_Track_Back_End/main_app/views.py
@@ -22,15 +22,18 @@ def insert_data(request):
 @api_view(['GET', 'POST'])
 def start_list(request):
     if request.method == 'GET':
-        starts = Start.objects.all() 
-        serializer = StartSerializer(starts, many = True)
-        return JsonResponse(serializer.data, safe = False)
+        starts = Start.objects.all()
+        serializer = StartSerializer(starts, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
     if request.method == 'POST':
-       serializer = StartSerializer(data = request.data) 
-       if serializer.is_valid():
-           serializer.save()
-           return Response(serializer.data, status = status.HTTP_201_CREATED)
+        serializer = StartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
            
 #note only use PUT when updating an existing record and PUT and DELETE follow same URL pattern 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
