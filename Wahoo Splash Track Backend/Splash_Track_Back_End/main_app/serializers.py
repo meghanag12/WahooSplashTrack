23cde7d560553bb46c473cffd581a97142c3c07c
Preston <xew5be@virginia.edu>
2024-10-09 21:06:50 -0400

    reverted serializers to fix GET on Starts

--- Wahoo Splash Track Backend/Splash_Track_Back_End/main_app/serializers.py
+++ Wahoo Splash Track Backend/Splash_Track_Back_End/main_app/serializers.py
@@ -6,13 +6,8 @@ class SwimmerSerializer(serializers.ModelSerializer):
         model = Swimmer
         fields = ['swimmer_id', 'first_name', 'last_name', 'year', 'active']
 
-class ForceSerializer(serializers.Serializer):
-    force = serializers.FloatField()
-
 class StartSerializer(serializers.ModelSerializer):
     swimmer = SwimmerSerializer()  # Nested SwimmerSerializer
-    front_force = ForceSerializer(many=True)  # Array of forces
-    back_force = ForceSerializer(many=True)   # Array of forces
 
     class Meta:
         model = Start
