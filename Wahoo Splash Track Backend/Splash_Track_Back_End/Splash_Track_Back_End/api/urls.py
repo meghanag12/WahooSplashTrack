b12a83e95fe18fb1b681be7a19de447a63134c9c
meghanag12 <112290030+meghanag12@users.noreply.github.com>
2024-11-06 17:43:44 -0500

    changes to the url set up

--- Wahoo Splash Track Backend/Splash_Track_Back_End/Splash_Track_Back_End/api/urls.py
+++ Wahoo Splash Track Backend/Splash_Track_Back_End/Splash_Track_Back_End/api/urls.py
@@ -1,11 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from main_app.api.urls import swimmer_router, start_router, myRio_router
+from main_app.api.views import SwimmerViewSet, StartViewSet, MyRioViewSet
 from django.urls import path, include
 
-router = DefaultRouter() 
-router.registry.extend(swimmer_router.registry)
-router.registry.extend(start_router.registry)
-router.registry.extend(myRio_router.registry)
+router = DefaultRouter()
+#rsswimmer argument specifies the URL prefix for the routes handled by this viewset
+#automatically creates API routes like /swimmer/ and /swimmer/{id}/
+router.register(r'swimmer', SwimmerViewSet)
+router.register(r'start', StartViewSet)
+router.register(r'myrio', MyRioViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
