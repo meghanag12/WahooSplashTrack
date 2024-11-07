6d20b13e9947f9b3a06a9a2f8e1e0720c99c57c7
meghanag12 <112290030+meghanag12@users.noreply.github.com>
2024-11-07 10:57:25 -0500

    changes i made

--- Wahoo Splash Track Backend/Splash_Track_Back_End/Splash_Track_Back_End/api/urls.py
+++ Wahoo Splash Track Backend/Splash_Track_Back_End/Splash_Track_Back_End/api/urls.py
@@ -1,6 +1,5 @@
 from rest_framework.routers import DefaultRouter
 from main_app.api.urls import swimmer_router, start_router, myRio_router
-from main_app.api.views import SwimmerViewSet, StartViewSet, MyRioViewSet
 from django.urls import path, include
 
 router = DefaultRouter() 
