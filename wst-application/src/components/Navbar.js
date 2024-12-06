587607ec651a41f0e05435aed2ce2eec19bccd6d
Preston <xew5be@virginia.edu>
2024-12-06 10:16:10 -0500

    Added some animation to navbar

--- wst-application/src/components/Navbar.js
+++ wst-application/src/components/Navbar.js
@@ -3,28 +3,38 @@ import { Link } from "react-router-dom"
 
 export function Navbar() {
     return (
-    <>
-        <div className = "navbar">
-            <Link to= "/reg">
-                <div className = "nav_button">
-                    <img src= {require("../register_icon.png")} alt="Icon" style={{ width: "120px", height: "100px", verticalAlign: "middle" }} />
-                </div>
-            </Link>
+        <>
+            <div className="navbar">
+                <Link to="/reg">
+                    <div className="nav_button">
+                        <img
+                            src={require("../register_icon.png")}
+                            alt="Icon"
+                            className="nav_icon"
+                        />
+                    </div>
+                </Link>
 
-            <Link to= "/magnituderecorder">
-                <div className = "nav_button">
-                    <img src= {require("../swim_icon.png")} alt="Icon" style={{ width: "120px", height: "110px", verticalAlign: "middle" }} />
-                </div>
-            </Link>
+                <Link to="/magnituderecorder">
+                    <div className="nav_button">
+                        <img
+                            src={require("../swim_icon.png")}
+                            alt="Icon"
+                            className="nav_icon"
+                        />
+                    </div>
+                </Link>
 
-            <Link to= "/progresstracker">
-                <div className = "nav_button">
-                    <img src= {require("../progress_icon.PNG")} alt="Icon" style={{ width: "120px", height: "100px", verticalAlign: "middle" }} />
-                </div>
-            </Link>
-            
-        </div>
-
-    </>
-    )
+                <Link to="/progresstracker">
+                    <div className="nav_button">
+                        <img
+                            src={require("../progress_icon.PNG")}
+                            alt="Icon"
+                            className="nav_icon"
+                        />
+                    </div>
+                </Link>
+            </div>
+        </>
+    );
 }
