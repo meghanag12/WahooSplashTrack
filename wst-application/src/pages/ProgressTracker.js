6cb76c6c28fe811aeddfb7b12e06c45490cbd16f
meghanag12 <112290030+meghanag12@users.noreply.github.com>
2025-03-24 14:41:41 -0400

    first change for the progress tracker ui update:

--- wst-application/src/pages/ProgressTracker.js
+++ wst-application/src/pages/ProgressTracker.js
@@ -42,36 +42,58 @@ export function ProgressTracker() {
         className="search-bar"
       />
     
-<div className="tracker-container">
+    <div className="tracker-container">
   {filteredSwimmers.length > 0 ? (
     <div className="update-container">
-      <table class ="table-striped table-bordered table-hover table-responsive-sm">
-        <caption> End of List of all Swimmers </caption>
+      {/* Optional Table version (commented out) */}
+      {/* <table className="table table-striped table-bordered table-hover table-responsive-sm">
+        <caption>End of List of all Swimmers</caption>
         <thead>
-          <tr class = "table-primary">
+          <tr className="table-primary">
             <th>Swimmer Name</th>
             <th>Graduation Year</th>
           </tr>
         </thead>
         <tbody>
           {filteredSwimmers.map(swimmer => (
-            <tr 
+            <tr
               key={swimmer.id}
               onClick={() => navigate(`/swimmer/${swimmer.swimmer_name}`)}
               style={{ cursor: "pointer" }}
             >
-              <strong><td>{swimmer.swimmer_name}</td></strong>
+              <td><strong>{swimmer.swimmer_name}</strong></td>
               <td><strong>{swimmer.year}</strong></td>
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
+
+      {/* Bootstrap Cards Version */}
+      <div className="container mt-4">
+        <div className="row row-cols-1 row-cols-md-1 g-4 d-flex justify-content-center">
+          {filteredSwimmers.map((swimmer, index) => (
+            <div
+              className="col"
+              key={swimmer.id}
+              onClick={() => navigate(`/swimmer/${swimmer.swimmer_name}`)}
+              style={{ cursor: "pointer" }}
+            >
+              <div className="card h-100 w-100" style = {{backgroundColor: index%2 === 0 ? "#3B4A88" : "#E57200", width: "300px" }}>
+                <div className="card-body">
+                  <h5 className="card-title">{swimmer.swimmer_name}</h5>
+                  <p className="card-text">{swimmer.year}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-    
   ) : (
     <p>No swimmers found</p>
   )}
 </div>
+
 
     </div>
     </div>
