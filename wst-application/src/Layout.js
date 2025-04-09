3a877661699e5b65893ab39e9ca581f172e7e004
Andy Chen <njx7sq@virginia.edu>
2025-04-09 10:32:02 -0400

    connection status UI

--- wst-application/src/Layout.js
+++ wst-application/src/Layout.js
@@ -1,10 +1,12 @@
 import { Navbar } from "./components/Navbar"
 import { Outlet } from "react-router-dom"
+import { ConnectionStatus } from "./components/ConnectionStatus"
 
 export function Layout() {
     return (
         <>
             <Navbar />
+            <ConnectionStatus />
             <main>
                 {/* renders all child routes, so layout can 
                 be made as the parent to all child routes */}
