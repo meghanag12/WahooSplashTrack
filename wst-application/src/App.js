eefe9bbb39b3bd4f10bd0f9f0cacaefc1a084090
Preston <xew5be@virginia.edu>
2025-04-10 10:51:18 -0400

    Login FLow working

--- wst-application/src/App.js
+++ wst-application/src/App.js
@@ -12,7 +12,42 @@ import { LoginPage } from './pages/LoginPage';
 import { ManualEntry } from './pages/ManualEntry';
 import {Navbar} from './components/Navbar'
 
+import { useEffect } from "react";
+import { redirectToLogin, getCodeFromUrl } from "./utils/auth";
+
 function App() {
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    const code = getCodeFromUrl();
+
+    // If no access token and no code, send user to login
+    if (!token && !code) {
+      redirectToLogin();
+    }
+
+    // If we have a code but no token yet, send to backend (lambda) to exchange it
+    if (code && !token) {
+      fetch(`https://oqoe7orlk2.execute-api.us-east-1.amazonaws.com/default/login_routine`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.access_token) {
+            localStorage.setItem("access_token", data.access_token);
+            // You might want to store id_token or refresh_token too
+            window.history.replaceState({}, document.title, "/"); // remove code from URL
+          } else {
+            console.error("Failed to get token:", data);
+            redirectToLogin(); // fallback
+          }
+        })
+        .catch((err) => {
+          console.error("Login error:", err);
+          redirectToLogin();
+        });
+    }
+  }, []);
+
+
   return (
     <Router>
       <Routes>
---
+++ wst-application/src/utils/auth.js
@@ -0,0 +1,16 @@
+export const COGNITO_DOMAIN = "https://us-east-1od0fs3uux.auth.us-east-1.amazoncognito.com";
+export const CLIENT_ID = "7jl1f1vdt300c1g7kt235g2ofn";
+export const REDIRECT_URI = "https://wahoosplashtrack.com/"; // your actual domain
+export const RESPONSE_TYPE = "code";
+export const SCOPES = "email openid phone";
+export const redirectToLogin = () => {
+    const loginUrl = `${COGNITO_DOMAIN}/login?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+      REDIRECT_URI
+    )}&response_type=${RESPONSE_TYPE}&scope=${encodeURIComponent(SCOPES)}`;
+  
+    window.location.href = loginUrl;
+  };
+  
+  export const getCodeFromUrl = () => {
+    return new URLSearchParams(window.location.search).get("code");
+  };
